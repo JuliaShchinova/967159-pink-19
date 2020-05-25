2a9ea9edd1e9241059519872729d95da6364007c
Julia <sablina83@yandex.ru>
2020-05-25 10:51:56 +0300

    проект

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,16 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
 var objectFit = ('objectFit' in document.documentElement.style === false) ? false : true;
 
 var makeObjectFit = function (img, type) {
@@ -14,3 +27,5 @@ if (!objectFit) {
     var image = document.querySelector('.photos__link-image--panorama');
     makeObjectFit(image, 'cover');
 }
+
+
